a6f04016e946b6c031ba7f4e45d5440f9eb71b00
Peter Ngacha <96945268+Peterngacha85@users.noreply.github.com>
2023-10-05 11:04:49 +0300

    Update App.js COMMENT

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Marks from './components/marks.jsx'
 function App() {
   const [scores, setScores] = useState(0);
   return (
+    //COMMENT
     <div className="App">
       <Marks scores={scores} setScores={setScores} />
        <Questions scores={scores} setScores={setScores} />
